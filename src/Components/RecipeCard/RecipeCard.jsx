641568b60e1335838a1b5ce61323233d54890533
Paul Spencer <billsongames@gmail.com>
2024-04-23 22:55:45 +0100

    testing recipe card

--- src/Components/RecipeCard/RecipeCard.jsx
+++ src/Components/RecipeCard/RecipeCard.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+
+import ToAddToShoppingList from "../ToAddToShoppingList/ToAddToShoppingList";
 
 import { test_recipe } from "../../data/recipes"
 
@@ -8,30 +10,75 @@ const RecipeCard = () => {
 
   const recipe = test_recipe.meals[0]
 
-    return (
-        <section>
-            <h2>
-                {recipe.strMeal}
-            </h2>
-            <img className="recipe-card-image" src={recipe.strMealThumb} />
-              <ul>
-                <li className="recipe-card-ingredients-list-entry">
-                    <div className="recipe-card-ingredients-list-entry-measure">
-                        {recipe.strMeasure1}
-                    </div>
-                    <div className="recipe-card-ingredients-list-entry-ingredient">
-                        {recipe.strIngredient1}
-                    </div>
-                    <div>
-                        <input type="checkbox" id={recipe.strIngredient1} />
-                    </div>
+  const [toAddtoList, setToAddtoList] = useState([])
 
-                </li>
 
-            </ul>
+  const handleIngredientToggle = () => {
+    const form = document.getElementById("ingredientSelector")
+    const inputs = form.getElementsByTagName("input")
+    let ingredientsArray =[]
+    
 
-        </section>
-    )
+    for (var i=0, max=inputs.length; i < max; i += 1) {
+      if (inputs[i].type === "checkbox" && inputs[i].checked) {
+        ingredientsArray.push(inputs[i].value)
+      }
+    setToAddtoList(ingredientsArray)  
+    }
+
+
+  }
+
+  return (
+    <section>
+      <h2>
+        {recipe.strMeal}
+      </h2>
+      <img className="recipe-card-image" src={recipe.strMealThumb} />
+      <form id="ingredientSelector">
+      {recipe.strIngredient1 !== null
+      ?
+      <ul>
+        <li className="recipe-card-ingredients-list-entry">
+          <div className="recipe-card-ingredients-list-entry-measure">
+            {recipe.strMeasure1}
+          </div>
+          <div className="recipe-card-ingredients-list-entry-ingredient">
+            {recipe.strIngredient1}
+          </div>
+          <input type="checkbox"
+            value={recipe.strIngredient1}
+            onClick={handleIngredientToggle}
+          />
+        </li>
+      </ul>
+      : <></>
+      }
+
+      {recipe.strIngredient2 !== null
+      ?
+      <ul>
+        <li className="recipe-card-ingredients-list-entry">
+          <div className="recipe-card-ingredients-list-entry-measure">
+            {recipe.strMeasure2}
+          </div>
+          <div className="recipe-card-ingredients-list-entry-ingredient">
+            {recipe.strIngredient2}
+          </div>
+          <input type="checkbox"
+            value={recipe.strIngredient2}
+            onClick={handleIngredientToggle}
+          />
+        </li>
+      </ul>
+      : <></>
+      }
+      </form>
+
+      {toAddtoList}
+    </section>
+
+  )
 }
 
 export default RecipeCard
---
+++ src/Components/ToAddToShoppingList/ToAddToShoppingList.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ToAddToShoppingList = ({toAddtoList}) => {
+  return(
+    <section>
+      <h3>
+        To Add To Shopping List
+      </h3>
+      <div>
+        {toAddtoList}
+      </div>
+
+    </section>
+  )
+}
+
+export default ToAddToShoppingList
