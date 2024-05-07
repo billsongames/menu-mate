2cf536f26b14b169860d7717122d4ce8c2b8f565
billsongames <billsongames@gmail.com>
2024-05-07 12:24:57 +0100

    cleanup data folder

--- src/data/recipe-descriptions.js
+++
@@ -1,4 +0,0 @@
-export const recipeDescriptions = {
-  "52874" : "Slow cooked beef makes a wonderful filling for pie. Prepare the meat stew ahead and assemble the pie when you’re ready to cook it.",
-  
-}
