7923e0dda3580818c9e7389d024c229083fde2ad
billsongames <billsongames@gmail.com>
2024-05-30 00:14:15 +0100

    added home page text

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -30,18 +30,15 @@ const Home = () => {
   ]
   return (
     <div className="home-container">
-      <ImageList sx={{ width: 1280, height: 720 }} cols={2} rowHeight={360}>
-        {imageData.map((image) => (
-          <ImageListItem key={image.img}>
-            <img
-              srcSet={`${image.img}?w=640&h=360&fit=crop&auto=format&dpr=2 2x`}
-              src={`${image.img}?w=640&h=360&fit=crop&auto=format`}
-              alt={image.alt}
-              loading="lazy"
-            />
-          </ImageListItem>
-        ))}
-      </ImageList>
+      <div>
+        <img src={imageData[1].img} className="home-image" />
+        <div id="home-text">
+          <div>Add the recipes</div>
+          <div>Finish the menu</div>
+          <div>Remove the stress</div>
+        </div>
+      </div>
+
     </div>
   )
 }
