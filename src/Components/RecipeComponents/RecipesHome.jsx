29296facbf603656745a2c5d6b99744c52f4bcd7
billsongames <billsongames@gmail.com>
2024-05-29 23:52:55 +0100

    recipe selection cards styled correctly

--- src/Components/RecipeComponents/RecipesHome.jsx
+++ src/Components/RecipeComponents/RecipesHome.jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useRef, useState } from "react";
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
+import CardActionArea from "@mui/material/CardActionArea";
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
@@ -93,35 +94,53 @@ const RecipesHome = () => {
                 margin: "1em",
                 maxWidth: "24em",
                 minWidth: "24em",
-                height: "24em"
+                height: "24em",
               }}
               data-recipelink={recipe.recipe.uri}
             >
-              <CardMedia
-                className="recipe-selection-card-link"
-                component="img"
-                alt={recipe.recipe.label}
-                height="200"
-                image={recipe.recipe.images.REGULAR.url}
-                data-recipelink={recipe.recipe.uri}
-                onClick={handleOpenRecipeCard}
-              />
-              <CardContent>
-                <Typography variant="h6" gutterBottom className="recipe-selection-card-link" data-recipelink={recipe.recipe.uri} onClick={handleOpenRecipeCard}>
-                  {recipe.recipe.label}
-                </Typography>
-              </CardContent>
-              <CardActions
-                sx={{
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "right",
-                  alignItems: "end",
-                }}
+              <div className="recipe-selection-details-container">
+                <CardMedia
+                  sx={{
+                    cursor: "pointer"
+                  }}
+                  component="img"
+                  alt={recipe.recipe.label}
+                  height="200"
+                  image={recipe.recipe.images.REGULAR.url}
+                  data-recipelink={recipe.recipe.uri}
+                  onClick={handleOpenRecipeCard}
+                />
+                <CardContent>
+                  <Typography
+                    gutterBottom
+                    variant="h6"
+                    sx={{
+                      textAlign: "left",
+                      cursor: "pointer"
+                    }}
+                    data-recipelink={recipe.recipe.uri}
+                    onClick={handleOpenRecipeCard}
+                  >
+                    {recipe.recipe.label}
+                  </Typography>
+                </CardContent>
+                <CardActions
+                  sx={{
+                    display: "flex",
+                    justifyContent: "end"
+                  }}
 
-              >
-                <Button size="small" data-recipelink={recipe.recipe.uri} onClick={handleOpenRecipeCard}>View details</Button>
-              </CardActions>
+                >
+                  <Button
+                    size="small"
+                    data-recipelink={recipe.recipe.uri}
+                    onClick={handleOpenRecipeCard}
+                  >
+                    View details
+                  </Button>
+                </CardActions>
+              </div>
+
             </Card>
           ))}
 
