80106af84eb73847d63d3270aa546409d4789baa
billsongames <billsongames@gmail.com>
2024-06-03 23:23:20 +0100

    carousel and buttons working

--- src/Components/Header/NavBarRegion.jsx
+++ src/Components/Header/NavBarRegion.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom';
 
+import Carousel from 'react-material-ui-carousel'
+import { Paper, Button } from '@mui/material'
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+
 import { Typography } from "@mui/material";
 
 import "./navbarRegion.css";
@@ -10,19 +15,25 @@ const NavBarRegion = () => {
 
   const navigate = useNavigate()
 
-  const regions = [
+  const regionsOne = [
     "American",
     "Asian",
     "British",
     "Caribbean",
     "Central Europe",
     "Chinese",
+  ]
+
+  const regionsTwo = [
     "Eastern Europe",
     "French",
     "Indian",
     "Italian",
     "Japanese",
-    "Kosher",
+    "Kosher"
+  ]
+
+  const regionsThree =[
     "Mediterranean",
     "Mexican",
     "Middle Eastern",
@@ -40,9 +51,31 @@ const NavBarRegion = () => {
   return (
     <nav className="region-button-container">
       <Typography variant="h5">Recipes by region</Typography>
-      {regions.map((region) => (
-        <button key={region} className="region-button" data-region={region} onClick={handleClick}>{region}</button>
-      ))}
+      <Carousel
+        autoPlay={false}
+        animation="slide"
+        NavButton={({ onClick, className, style, next, prev }) => {
+          // Other logic
+
+          return (
+            <Button onClick={onClick} className={className} style={style}>
+              {next && <ArrowForwardIosIcon sx={{color: "#8FBA74"}} />}
+              {prev && <ArrowBackIosIcon sx={{color: "#8FBA74"}} />}
+            </Button>
+          )
+        }}
+      >
+        <div>
+          {regionsOne.map((region) => (
+            <button key={region} className="region-button" data-region={region} onClick={handleClick}>{region}</button>
+          ))}
+        </div>
+        <div>
+          {regionsTwo.map((region) => (
+            <button key={region} className="region-button" data-region={region} onClick={handleClick}>{region}</button>
+          ))}
+        </div>
+      </Carousel>
     </nav>
   )
 }
