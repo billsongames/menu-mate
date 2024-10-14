f6213a6d3b5fee9c64524551da71e32247b9eb4d
Paul Spencer <billsongames@gmail.com>
2024-10-14 23:22:23 +0100

    added gluten free to recipes menu

---
+++ src/Components/RecipeContainers/RecipesGlutenFreeContainer.jsx
@@ -0,0 +1,26 @@
+import React, { useContext, useEffect } from "react";
+
+import { DisplayModeContext } from "../../context/DisplayModeContext"
+
+import HeaderContainer from "../Header/HeaderContainer";
+
+import RecipesGlutenFree from "../RecipeComponents/RecipesGlutenFree";
+
+const RecipesGlutenFreeContainer = () => {
+  const {displayMode, toggleDisplayMode} = useContext(DisplayModeContext)
+
+  useEffect(() => {
+    toggleDisplayMode("recipes")
+  },[])
+
+  return (
+    <React.Fragment>
+      <HeaderContainer />
+      <main className="main">
+        <RecipesGlutenFree />
+      </main>
+    </React.Fragment>
+  )
+}
+
+export default RecipesGlutenFreeContainer
